2d5fcb303b54f58462cbc8b8b9d978cb6023eeb8
Paulo <paulo.alexander@aluno.ufabc.edu.br>
2019-06-24 01:50:56 -0300

    Add threads

--- distributed_systems_0/src/UDP_Client.java
+++ distributed_systems_0/src/UDP_Client.java
@@ -1,4 +1,24 @@
 
-public class UDP_Client {
 
+import java.io.*;
+import java.net.*;
+
+class UDPClient implements runnable
+
+{
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
+      InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+      String sentence = inFromUser.readLine();
+      sendData = sentence.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+      clientSocket.send(sendPacket);
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      clientSocket.receive(receivePacket);
+      String modifiedSentence = new String(receivePacket.getData());
+      System.out.println("Do servidor:" + modifiedSentence);
+      clientSocket.close();
+   
 }
--- distributed_systems_0/src/UDP_MAIN.java
+++ distributed_systems_0/src/UDP_MAIN.java
@@ -1,10 +1,18 @@
 
+import java.lang.Thread;
+
 public class UDP_MAIN {
 
-	public static void main (String args[])
+	
+	public static void main(String args[]) throws Exception
 	{
+		String Thread_One = "Teste1";
+		String Thread_Two = "Verdadeiro";
+		Thread t = new ThreadDemo(Thread_One);
+		Thread t1 = new ThreadDemo(Thread_Two);
+		t.start();
 		
-		System.out.println("Teste");
+		
 	}
 	
 	
